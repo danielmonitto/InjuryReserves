c12ba6f37c2e22e8641ed6a193cb0d180aa171f6
danielmonitto <danielmonitto@hotmail.com>
2026-05-04 09:26:41 +1000

    s4 g10

--- app.js
+++ app.js
@@ -127,8 +127,8 @@ const GAME_VIDEOS = {
   "4_6": { highlights: "", full: "" },
   "4_7": { highlights: "https://youtu.be/BN0H8gIA9aQ", full: "https://youtu.be/7yngkGXZH5o" },
   "4_8": { highlights: "https://youtu.be/twLG1McLIQg", full: "https://youtu.be/kRq9yIn1_UY" },
-  "4_9": { highlights: "", full: "https://youtu.be/qzpghVnK2NA" },
-  "4_10": { highlights: "", full: "" },
+  "4_9": { highlights: "https://youtu.be/bwLuDiQcGpM", full: "https://youtu.be/qzpghVnK2NA" },
+  "4_10": { highlights: "https://youtu.be/h23-OEiTI9Y", full: "https://youtu.be/VflR5_CPkIk" },
   "4_11": { highlights: "", full: "" },
   "4_12": { highlights: "", full: "" },
 
